7763aa01266640a1cb59f58a9630b8893d5abea5
hvgiang86 <hvgiang86@gmail.com>
2022-09-20 17:43:11 +0700

    add Data Binding to Recycler View

--- app/src/main/java/com/example/vietlottdatacrawl/adapter/Max3DResultAdapter.java
+++ app/src/main/java/com/example/vietlottdatacrawl/adapter/Max3DResultAdapter.java
@@ -1,15 +1,19 @@
 package com.example.vietlottdatacrawl.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableField;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vietlottdatacrawl.R;
+import com.example.vietlottdatacrawl.databinding.SessionItemLayoutBinding;
 import com.example.vietlottdatacrawl.model.PrizeDrawSession;
 
 import java.util.List;
@@ -20,7 +24,6 @@ public class Max3DResultAdapter extends RecyclerView.Adapter<Max3DResultAdapter.
     private static final int RESULT_FOUND_VIEW_TYPE = 10;
     private static final int RESULT_NOT_FOUND_VIEW_TYPE = 11;
     private final Context context;
-    private final int[] textViewIdArray = new int[60];
 
     //Item List Dataset
     private List<PrizeDrawSession> sessionList;
@@ -28,7 +31,6 @@ public class Max3DResultAdapter extends RecyclerView.Adapter<Max3DResultAdapter.
     public Max3DResultAdapter(List<PrizeDrawSession> sessionList, Context context) {
         this.sessionList = sessionList;
         this.context = context;
-        convertTextViewIdToIdArray();
     }
 
     public void setSessionList(List<PrizeDrawSession> sessionList) {
@@ -37,47 +39,57 @@ public class Max3DResultAdapter extends RecyclerView.Adapter<Max3DResultAdapter.
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public View view;
+        SessionItemLayoutBinding binding;
+        public ObservableField<String> id = new ObservableField<>();
+        public ObservableField<String> dateStr = new ObservableField<>();
+        public ObservableArrayList<String> prizeNumber = new ObservableArrayList<>();
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.view = itemView;
+        public ViewHolder(SessionItemLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.view = binding.getRoot();
+        }
+
+        public void setBinding(String id, String dateStr, Byte[] prizeNumber) {
+            if (binding.getItemView() == null)
+                binding.setItemView(this);
+
+            for (int i = 0; i < 60; i++) {
+                String s = String.format(Locale.US, "%d", prizeNumber[i]);
+                this.prizeNumber.add(s);
+            }
+            this.id.set(id);
+            this.dateStr.set(dateStr);
+
+            binding.executePendingBindings();
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v;
-        if (viewType == RESULT_NOT_FOUND_VIEW_TYPE)
-            v = inflater.inflate(R.layout.result_not_found_layout, parent, false);
-        else
-            v = inflater.inflate(R.layout.session_item_layout, parent, false);
+        SessionItemLayoutBinding binding;
 
-        return new ViewHolder(v);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (viewType == RESULT_NOT_FOUND_VIEW_TYPE)
+            binding = DataBindingUtil.inflate(inflater,R.layout.result_not_found_layout, parent, false);
+        else
+            binding = DataBindingUtil.inflate(inflater,R.layout.session_item_layout, parent, false);
+
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
         if (getItemViewType(position) != RESULT_NOT_FOUND_VIEW_TYPE) {
             PrizeDrawSession session = sessionList.get(position);
-
-            TextView dateTextView = holder.view.findViewById(R.id.session_date);
-            TextView idTextView = holder.view.findViewById(R.id.session_id);
-
-            //set text view content for id and date
-            dateTextView.setText(session.getDateString());
             String drawSessionId = "Kỳ quay số: #" + session.getId();
-            idTextView.setText(drawSessionId);
-            
-            //set content for each prize number ball
+            String dateStr = session.getDateString();
             Byte[] prizeNumber = session.getPrizeNumber();
-            for (int i = 1; i <= 60; ++i) {
-                int id = getTextViewId(i);
-                TextView textView = holder.view.findViewById(id);
-                String numberString = String.format(Locale.US, "%d", prizeNumber[i - 1]);
-                textView.setText(numberString);
-            }
+
+            holder.setBinding(drawSessionId,dateStr,prizeNumber);
         }
     }
 
@@ -94,73 +106,5 @@ public class Max3DResultAdapter extends RecyclerView.Adapter<Max3DResultAdapter.
         if (sessionList.size() == 0)
             return 1;
         return sessionList.size();
-    }
-
-    private int getTextViewId(int textViewIndex) {
-        return textViewIdArray[textViewIndex - 1];
-    }
-
-    private void convertTextViewIdToIdArray() {
-        int i = 0;
-        textViewIdArray[i++] = R.id.prize_number_1;
-        textViewIdArray[i++] = R.id.prize_number_2;
-        textViewIdArray[i++] = R.id.prize_number_3;
-        textViewIdArray[i++] = R.id.prize_number_4;
-        textViewIdArray[i++] = R.id.prize_number_5;
-        textViewIdArray[i++] = R.id.prize_number_6;
-        textViewIdArray[i++] = R.id.prize_number_7;
-        textViewIdArray[i++] = R.id.prize_number_8;
-        textViewIdArray[i++] = R.id.prize_number_9;
-        textViewIdArray[i++] = R.id.prize_number_10;
-        textViewIdArray[i++] = R.id.prize_number_11;
-        textViewIdArray[i++] = R.id.prize_number_12;
-        textViewIdArray[i++] = R.id.prize_number_13;
-        textViewIdArray[i++] = R.id.prize_number_14;
-        textViewIdArray[i++] = R.id.prize_number_15;
-        textViewIdArray[i++] = R.id.prize_number_16;
-        textViewIdArray[i++] = R.id.prize_number_17;
-        textViewIdArray[i++] = R.id.prize_number_18;
-        textViewIdArray[i++] = R.id.prize_number_19;
-        textViewIdArray[i++] = R.id.prize_number_20;
-        textViewIdArray[i++] = R.id.prize_number_21;
-        textViewIdArray[i++] = R.id.prize_number_22;
-        textViewIdArray[i++] = R.id.prize_number_23;
-        textViewIdArray[i++] = R.id.prize_number_24;
-        textViewIdArray[i++] = R.id.prize_number_25;
-        textViewIdArray[i++] = R.id.prize_number_26;
-        textViewIdArray[i++] = R.id.prize_number_27;
-        textViewIdArray[i++] = R.id.prize_number_28;
-        textViewIdArray[i++] = R.id.prize_number_29;
-        textViewIdArray[i++] = R.id.prize_number_30;
-        textViewIdArray[i++] = R.id.prize_number_31;
-        textViewIdArray[i++] = R.id.prize_number_32;
-        textViewIdArray[i++] = R.id.prize_number_33;
-        textViewIdArray[i++] = R.id.prize_number_34;
-        textViewIdArray[i++] = R.id.prize_number_35;
-        textViewIdArray[i++] = R.id.prize_number_36;
-        textViewIdArray[i++] = R.id.prize_number_37;
-        textViewIdArray[i++] = R.id.prize_number_38;
-        textViewIdArray[i++] = R.id.prize_number_39;
-        textViewIdArray[i++] = R.id.prize_number_40;
-        textViewIdArray[i++] = R.id.prize_number_41;
-        textViewIdArray[i++] = R.id.prize_number_42;
-        textViewIdArray[i++] = R.id.prize_number_43;
-        textViewIdArray[i++] = R.id.prize_number_44;
-        textViewIdArray[i++] = R.id.prize_number_45;
-        textViewIdArray[i++] = R.id.prize_number_46;
-        textViewIdArray[i++] = R.id.prize_number_47;
-        textViewIdArray[i++] = R.id.prize_number_48;
-        textViewIdArray[i++] = R.id.prize_number_49;
-        textViewIdArray[i++] = R.id.prize_number_50;
-        textViewIdArray[i++] = R.id.prize_number_51;
-        textViewIdArray[i++] = R.id.prize_number_52;
-        textViewIdArray[i++] = R.id.prize_number_53;
-        textViewIdArray[i++] = R.id.prize_number_54;
-        textViewIdArray[i++] = R.id.prize_number_55;
-        textViewIdArray[i++] = R.id.prize_number_56;
-        textViewIdArray[i++] = R.id.prize_number_57;
-        textViewIdArray[i++] = R.id.prize_number_58;
-        textViewIdArray[i++] = R.id.prize_number_59;
-        textViewIdArray[i] = R.id.prize_number_60;
     }
 }
